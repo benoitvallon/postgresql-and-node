5cf3feaec7c667fb40372705778c6d8757eb19e0
Benoit VALLON <benoitvallon@gmail.com>
2015-02-23 16:27:11 +0100

    Adds the delete method to the API

--- routes/index.js
+++ routes/index.js
@@ -112,4 +112,40 @@ router.put('/api/v1/todos/:todo_id', function(req, res) {
 
 });
 
+router.delete('/api/v1/todos/:todo_id', function(req, res) {
+
+    var results = [];
+
+    // Grab data from the URL parameters
+    var id = req.params.todo_id;
+
+    // Get a Postgres client from the connection pool
+    pg.connect(connectionString, function(err, client, done) {
+
+        // SQL Query > Delete Data
+        client.query("DELETE FROM items WHERE id=($1)", [id]);
+
+        // SQL Query > Select Data
+        var query = client.query("SELECT * FROM items ORDER BY id ASC");
+
+        // Stream results back one row at a time
+        query.on('row', function(row) {
+            results.push(row);
+        });
+
+        // After all data is returned, close connection and return results
+        query.on('end', function() {
+            client.end();
+            return res.json(results);
+        });
+
+        // Handle Errors
+        if(err) {
+          console.log(err);
+        }
+
+    });
+
+});
+
 module.exports = router;
