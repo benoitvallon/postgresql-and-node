0b85705b92a8acc400d9262d5e76f3c6cd249321
Benoit VALLON <benoitvallon@gmail.com>
2015-02-23 17:10:54 +0100

    Adds the main view of our angular app

---
+++ public/javascripts/app.js
@@ -0,0 +1,17 @@
+angular.module('nodeTodo', [])
+
+.controller('mainController', function($scope, $http) {
+
+    $scope.formData = {};
+    $scope.todoData = {};
+
+    // Get all todos
+    $http.get('/api/v1/todos')
+        .success(function(data) {
+            $scope.todoData = data;
+            console.log(data);
+        })
+        .error(function(error) {
+            console.log('Error: ' + error);
+        });
+});
--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,13 @@
 var express = require('express');
 var router = express.Router();
+var path = require('path');
+
 var pg = require('pg');
 var connectionString = process.env.DATABASE_URL || 'postgres://localhost:5432/todo';
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+    res.sendFile(path.join(__dirname, '../views', 'index.html'));
 });
 
 router.post('/api/v1/todos', function(req, res) {
