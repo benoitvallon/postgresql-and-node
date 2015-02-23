b64319e3442d8ab2a859d8c1eddd3c4f4d81107d
Benoit VALLON <benoitvallon@gmail.com>
2015-02-23 17:22:12 +0100

    Adds the create feature

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -27,4 +27,17 @@ angular.module('nodeTodo', [])
             });
     };
 
+    // Create a new todo
+    $scope.createTodo = function(todoID) {
+    $http.post('/api/v1/todos', $scope.formData)
+        .success(function(data) {
+            $scope.formData = {};
+            $scope.todoData = data;
+            console.log(data);
+        })
+        .error(function(error) {
+            console.log('Error: ' + error);
+        });
+};
+
 });
